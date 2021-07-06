b87d22c1121d32cda7c10df215674d034fba8d94
Isuri Nawanjanee <isujayawardana1220@gmail.com>
2021-07-06 23:49:27 +0530

    Add js

--- Js/register.js
+++ Js/register.js
@@ -4,6 +4,6 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', function(e)){
-    
-}
+form.addEventListener('submit', function(e){
+
+});
