d306c5846c2ddecc2177a1b5ae3a858f7424ce69
Isuri Nawanjanee <isujayawardana1220@gmail.com>
2021-07-07 19:13:57 +0530

    Change

--- Js/register.js
+++ Js/register.js
@@ -4,6 +4,45 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//Show Input Error Message
+function showError(input,message){
+    const formcontrol = input.parentElement;
+    formcontrol.className = 'form-control error';
+    const small = formcontrol.querySelector('small');
+    small.innerText = message;
+}
+
+//Show Input Success
+function showSuccess(input,message){
+    const formcontrol = input.parentElement;
+    formcontrol.className = 'form-control success';
+}
+
+//Check Email is Valid
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    
+}
+
 form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    //Username Details
+    if(username.value === ''){
+       showError(username, 'Username is required');
+    }
+    else{
+        showSuccess(username);
+    }
+
+    //Email Details
+    if(email.value === ''){
+        showError(email, 'Email is required');
+     }
+     else{
+         showSuccess(email);
+     }
+ 
 
 });
