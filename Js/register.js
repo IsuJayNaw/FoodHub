b85687778e9052f524ab7f4de8d93b2e02d9c3b8
Isuri Nawanjanee <isujayawardana1220@gmail.com>
2021-07-09 00:03:26 +0530

    Check length in password

--- Js/register.js
+++ Js/register.js
@@ -27,7 +27,7 @@ function checkEmail(input){
 function checkRequired(inputArr){
     inputArr.forEach(function(input){
        if(input.value.trim() === ''){
-            showError(input,`${getFieldName(input)} is required`)
+            showError(input,`${getFieldName(input)} is required`);
        }else{
            showSuccess(input);
        }
@@ -36,7 +36,18 @@ function checkRequired(inputArr){
 
 //Get field 
 function getFieldName(input){
-    return input.id.charAt(0).toUpperCase();
+    return input.id.charAt(0).toUpperCase( ) + input.id.slice(1);
+}
+
+//Check input Length
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    }else{
+        showSuccess(input);
+    }
 }
 
 form.addEventListener('submit', function(e){
@@ -44,4 +55,6 @@ form.addEventListener('submit', function(e){
 
     checkRequired([username, email, password, password2]);
     
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
 });
